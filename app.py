d805b9b1bba77044fdf8d535f0602c999c4d01ce
asas1231 <A0244361@ulive.pccu.edu.tw>
2026-01-16 13:27:42 +0800

    ver 1.02

--- app.py
+++ app.py
@@ -12,17 +12,23 @@ backend = Aer.get_backend('qasm_simulator')
 def get_quantum_move():
     """透過量子電路產生隨機出拳"""
     qc = QuantumCircuit(2, 2)
-    qc.h([0, 1])  # 對兩個位元施加 Hadamard Gate
+    qc.h([0, 1])  # 施加 Hadamard Gate
     qc.measure([0, 1], [0, 1])
     
+    # 修正 Qiskit 1.0+ 的執行語法
+    job = backend.run(qc, shots=1) 
+    result = job.result().get_counts()
+    
     while True:
-        job = execute(qc, backend, shots=1)
-        result = job.result().get_counts()
-        outcome = list(result.keys())[0] # 得到 '00', '01', '10' 或 '11'
-        
+        outcome = list(result.keys())[0] 
         mapping = {"00": "石頭", "01": "剪刀", "10": "布"}
+        
         if outcome in mapping:
             return mapping[outcome]
+        else:
+            # 如果抽到 11，重新跑一次電路
+            job = backend.run(qc, shots=1)
+            result = job.result().get_counts()
 
 # --- 網頁介面與邏輯 ---
 st.set_page_config(page_title="量子猜拳大賽", layout="centered")
